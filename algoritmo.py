d6b2ea2be08f9874dbd86d388c5f4b43f83b5b3e
ThiagoZaneti <dotinha0203@gmail.com>
2024-05-06 07:39:48 -0300

    Pedindo nome para o usuário

--- algoritmo.py
+++ algoritmo.py
@@ -2,3 +2,6 @@ distancia = float(input("Escreva a distancia que percorreu"))
 velocidade = float(input("Escreva a velocidade que percorreu"))
 tempo = distancia/velocidade
 print(f"voce demorou {tempo}")
+
+nome = str(input("escreva seu nome"))
+print(nome)
