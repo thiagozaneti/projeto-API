82e5ad11ac4892b128508d71ed61771b334e0e9c
ThiagoZaneti <dotinha0203@gmail.com>
2024-05-07 08:14:04 -0300

    retirada do arquivo algoritmo.py

--- algoritmo.py
+++
@@ -1,7 +0,0 @@
-distancia = float(input("Escreva a distancia que percorreu"))
-velocidade = float(input("Escreva a velocidade que percorreu"))
-tempo = distancia/velocidade
-print(f"voce demorou {tempo}")
-
-nome = str(input("escreva seu nome"))
-print(nome)
