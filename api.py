39841941782aad4fe4276e2a82d03511f4a2b56b
ThiagoZaneti <dotinha0203@gmail.com>
2024-05-14 07:25:12 -0300

    arrumado

--- api.py
+++ api.py
@@ -1,51 +1,62 @@
-from flask import Blueprint, jsonify
-from funcoes import mostrar_lista, contador_de_idade, counter_salario, tres_maiores_salarios, calcular_media_salarios, imprimir_media, intervalo_de_idade
-from random_data import pessoas
+from flask import Blueprint, jsonify, request
 
 bp = Blueprint("api", __name__)
 
-# @bp.route("/api/index", methods = ["GET"])
-# def index():
-#     mostrar_pessoas = mostrar_lista(pessoas)
-#     return jsonify({"Status":200, "Pessoas":mostrar_pessoas})
+#Daqui pra baixo as rotas
 
-@bp.route("/api/idades", methods = ["GET"])
+@bp.route("/api", methods=("GET",))
+def index():
+    return jsonify({"status": 200, "data": "API do Aluno"})
+
+@bp.route("/api/aleatorios", methods=("GET",)) # decorator de rota
+def aleatorios(): # função python
+    import random
+    a = random.randint(49, 100)
+    return jsonify({"status": 200, "data": a}) # retorno
+
+@bp.route("/api/argumentos/<string:nome>", methods=("GET",))
+def argumentos(nome: str):
+    return jsonify({"status": 200, "data": nome})
+
+@bp.route("/api/argumentos", methods=("GET",))
+def arg_implicito():
+    return jsonify({"status": 200, "data": request.args["nome"]})
+
+@bp.route("/api/idades", methods=("GET",))
 def idades():
-    idades_maior_50 = contador_de_idade(pessoas)
-    return jsonify({"Status":200, "Pessoas com mais de 50 anos":idades_maior_50})
+    from random_data import pessoas
+    import funcoes
+    if request.args.get("sexo"):
+        idades = funcoes.maior_2000_sexo(pessoas, sexo="Masculino") # retorna profissões
+        homens = {"total": idades[0], "%":idades[1]}
+        idades = funcoes.maior_2000_sexo(pessoas, sexo="Feminino") # retorna profissões
+        mulheres = {"total": idades[0], "%":idades[1]}
 
-@bp.route("/api/salarios", methods = ["GET"])
+        return jsonify({"status": 200, "data": {"homens": homens, "mulheres":mulheres}})
+    else:
+        num = funcoes.maior_de_50(pessoas)
+        return jsonify({"status": 200, "data": num})
+
+@bp.route("/api/salarios", methods=("GET",))
 def salarios():
-    counter_salarios = counter_salario(pessoas)
-    contador_percent = counter_salarios[0]
-    total_percent = counter_salarios[1]
-    contador_total = counter_salarios[2]
-    return jsonify({"Status":200, "Salario maior que 2000": contador_percent, 
-                    "Total de pessoas":contador_total, 
-                    "porcentagem":total_percent })
+    from random_data import pessoas
+    import funcoes
+    num = funcoes.mais_2000(pessoas)
+    return jsonify({"status": 200, "data": {"registros": num[2], "porcentagem": num[1], "total": num[0]}})
 
-@bp.route("/api/maiores_salarios", methods = ["GET"])
-def maioresSalarios():
-    maiores_salarios = tres_maiores_salarios(pessoas)
-    return jsonify({"Status":200, "Três maiores salarios": maiores_salarios})
+@bp.route("/api/maiores_salarios", methods=("GET", ))
+def maiores_salarios():
+    from random_data import pessoas
+    import funcoes
+    pessoa1 = funcoes.maior_salario(pessoas)
+    pessoa2 = funcoes.maior_salario(pessoas, float(pessoa1['salario']))
+    pessoa3 = funcoes.maior_salario(pessoas, float(pessoa2['salario']))
+    return jsonify({"status": 200, "data": {"pessoa1": pessoa1
+                                , "pessoa2": pessoa2, "pessoa3": pessoa3}})
 
-@bp.route("/api/media_salarios", methods = ["GET"])
-def calcular_media_de_salarios():
-    media_salarios = calcular_media_salarios(pessoas)
-    return jsonify({"Status":200, "Media de salários":media_salarios})
-
-@bp.route("/api/medias", methods = ["GET"])
-def medias():
-    medias = imprimir_media(pessoas)
-    return jsonify({"Status":200, "Media de salários":medias})
-
-@bp.route("/api/intervalo_de_idade/<string:sexo>", methods = ["GET"])
-def intervalo_idade(sexo:str):
-    intervalos_de_idades = intervalo_de_idade(pessoas)
-    idade_media = intervalos_de_idades[0]
-    sexo_frequente = intervalos_de_idades[1]
-    sexo_menos_frequente = intervalos_de_idades[2]
-    if sexo == "f":
-        return jsonify({"Status":200,"Sexo menos frequente": sexo_menos_frequente})
-    elif sexo == "m":
-        return jsonify({"Status":200, "idades media": idade_media, "sexo frequente": sexo_frequente, "Sexo menos frequente": sexo_menos_frequente})
+@bp.route("/api/profissoes", methods=("GET", ))
+def profissoes():
+    from random_data import pessoas
+    import funcoes
+    profissoes = funcoes.media_profissoes(pessoas)
+    return jsonify({"status": 200, "data": profissoes})
